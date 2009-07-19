b5a174c7861e1ed887bfaf66fa88acd02ca9f65c
Joost Verdaasdonk <joost@1-2dance.nl>
2009-07-19 13:44:08 +0200

    Adding to person

--- MVC.Data/Model/Person.cs
+++ MVC.Data/Model/Person.cs
@@ -9,5 +9,7 @@ namespace CS.Data
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Hoby { get; set; }
     }
 }
