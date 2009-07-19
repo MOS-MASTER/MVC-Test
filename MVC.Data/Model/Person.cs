54f97806e02c1f79f54068ff338884f5b7dc2856
MOS MASTER <joost@1-2dance.nl>
2009-07-19 15:58:16 +0200

    Some props in Person are not needed for my test. :)

--- MVC.Data/Model/Person.cs
+++ MVC.Data/Model/Person.cs
@@ -9,7 +9,5 @@ namespace CS.Data
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string LastName { get; set; }
-        public string Hoby { get; set; }
     }
 }
