a41df081cc5426c13b0234dd7425abfbc149837a
Joost Verdaasdonk <joost@1-2dance.nl>
2009-07-19 13:40:32 +0200

    Added new model for person

---
+++ MVC.Data/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
